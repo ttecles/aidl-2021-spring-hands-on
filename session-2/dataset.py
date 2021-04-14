64e53ab268e077bff1e150d0c7e8c5feb019b46f
joan <joan.prat@knowtrade.eu>
2021-04-14 23:53:13 +0200

    WIP: doing model

--- session-2/dataset.py
+++ session-2/dataset.py
@@ -1,18 +1,25 @@
 import os
+from pathlib import Path
 
 import pandas as pd
 from torch.utils.data import Dataset
 from PIL import Image
 
+
 class MyDataset(Dataset):
 
     def __init__(self, images_path, labels_path, transform=None):
-        pass
-        
+        self.img_label = pd.read_csv(labels_path)
+        self.images_path = Path(images_path)
+        self.transform = transform
 
     def __len__(self):
-        pass
-
+        return len(self.img_label)
 
     def __getitem__(self, idx):
-        pass
+        row = self.img_label.loc[idx]
+        filename = f"input_{row.suite_id}_{row.sample_id}_{row.code}.jpg"
+        image = Image.open(self.images_path / filename)
+        if self.transform:
+            image = self.transform(image)
+        return {"label": row.code, "image": image}
--- session-2/model.py
+++ session-2/model.py
@@ -1,9 +1,32 @@
 import torch.nn as nn
 
+
 class MyModel(nn.Module):
 
     def __init__(self):
-        pass
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, out_channels=32, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(32, out_channels=64, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(64, out_channels=64, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.mlp = nn.Sequential(nn.Linear(3 * 3 * 64, 120),
+                                 nn.ReLU(),
+                                 nn.Linear(120, 84),
+                                 nn.ReLU(),
+                                 nn.Linear(84, 10),
+                                 nn.LogSoftmax(dim=1))
 
     def forward(self, x):
-        pass
+        x = self.conv(x)
+        bsz, nch, height, width = x.shape
+        x = x.reshape(bsz, -1)
+
+        y = self.mlp(x)
+        return y
